ac4e3f6c4df17513b488d5e590b85e4c731bb2b5
Haim5 <89258839+Haim5@users.noreply.github.com>
2022-04-26 10:57:52 +0300

    Update ContactsList.jsx

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,18 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import AddFriend from "./AddFriend";
 const ContactsList = ({user, clickHandle, getLastMessage, getUsernames, getNickname, addFriend}) => {
 
     const [buttonPopup, setButtonPopup] = useState(false);
-    const myFriends = user.contacts;
 
     const togglePopup = () => {
         setButtonPopup(!buttonPopup);
     }
-
-    useEffect(() => {
-        {<AddFriend trigger={buttonPopup} handleClose={togglePopup} getUsernames={getUsernames} getNickname={getNickname} friends={user.contacts + user.username} addFriend={addFriend} username={user.username}/> 
-    }
-    }, [myFriends]);
 
     const contacts = user && getLastMessage(user.username, user.contacts);
 
@@ -30,7 +24,7 @@ const ContactsList = ({user, clickHandle, getLastMessage, getUsernames, getNickn
                     <h2>{friend.nickname}</h2>
                     {friend.type === "text" && <h4>{friend.message}</h4>}
                     {friend.type !== "text" && <h4>{friend.type}</h4>}
-                    <img src={friend.photo} width="100" height="100"/>
+                    <img src={friend.photo} width="100" height="100" alt="profile-pic"/>
                     <div className="contact-preview-time">{friend.time}</div>
                 </div>
             ))}
